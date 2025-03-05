e038b35d400e484cb6b7d28a0bf477f8f930f71a
dfedyachkina <p87yeepz@students.codeinstitute.net>
2025-03-05 07:36:08 +0000

    Finish product size logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,15 +10,27 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
