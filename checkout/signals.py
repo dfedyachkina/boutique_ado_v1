11fd3899bc21b61e8381484f62234ceba6a133a1
dfedyachkina <p87yeepz@students.codeinstitute.net>
2025-03-14 16:25:50 +0000

    Add checkout success logic

--- checkout/signals.py
+++ checkout/signals.py
@@ -12,10 +12,11 @@ def update_on_save(sender, instance, created, **kwargs):
 
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     # chris made a mistake here, this function should have 
     # been called update_on_delete. He fixes this in an upcoming video.
     """
     Update order total on lineitem delete
     """
+    print('Delete signal received!')
     instance.order.update_total()
